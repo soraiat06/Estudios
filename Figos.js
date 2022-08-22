27232e4e55917eb7dcacd8056fc9800a1a18b97b
Soraia <101956001+soraiat06@users.noreply.github.com>
2022-08-22 17:28:00 -0300

    Update Figos.js

--- Figos.js
+++ Figos.js
@@ -1,24 +1,25 @@
-document.addEventListener("DOMContentLoaded", Entra_st ) 
+document.addEventListener('DOMContentLoaded', f_load)
 
-function Entra_st() {
-    const listaprincipal = document.createElement('ol')
-    document.body.prepend(listaprincipal)
+function f_load() {
+
+    const listaMae = document.createElement('ol')
+    document.body.prepend(listaMae)
     const contH2 = document.querySelectorAll('h2')
 
     for (let i = 0; i < contH2.length; i++) {
 
-        const elementos = f_criaçãodeLista(`#h2${i + 1}`, contH2[i].textContent)
-        listaprincipal.appendChild(elementos)
+        const elementos = f_criarLista(`#h2${i + 1}`, contH2[i].textContent)
+        listaMae.appendChild(elementos)
         mudarH2(contH2[i], i + 1)
 
     }
 }
 
 // Adicionando os h2 na lista (ol)
-function f_criaçãodeLista(ref, texto) {
+function f_criarLista(ref, texto) {
 
     const li = document.createElement('li')
-    const criarLink = f_criaçãodeLista(ref, texto)
+    const criarLink = f_criarLink(ref, texto)
     li.appendChild(criarLink)
 
     return li
@@ -47,6 +48,3 @@ function f_inserirDepois(elemento, ref) {
     const nextElement = elemento.nextElementSibling
     elemento.parentElement.insertBefore(ref, nextElement)
 }
-
-
-  
