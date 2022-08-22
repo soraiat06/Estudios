d564d1532296da2a842a1240e9628e4ad99668f0
Soraia <101956001+soraiat06@users.noreply.github.com>
2022-08-22 17:08:20 -0300

    Create Cofig.js

---
+++ Cofig.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", Entra_st ) 
+
+function Entra_st() {
+    const listaprincipal = document.createElement('ol')
+    document.body.prepend(listaprincipal)
+    const contH2 = document.querySelectorAll('h2')
+
+    for (let i = 0; i < contH2.length; i++) {
+
+        const elementos = f_criaçãodeLista(`#h2${i + 1}`, contH2[i].textContent)
+        listaprincipal.appendChild(elementos)
+        mudarH2(contH2[i], i + 1)
+
+    }
+}
+
+// Adicionando os h2 na lista (ol)
+function f_criaçãodeLista(ref, texto) {
+
+    const li = document.createElement('li')
+    const criarLink = f_criaçãodeLista(ref, texto)
+    li.appendChild(criarLink)
+
+    return li
+}
+
+// Criando texto clicável (link)
+function f_criarLink(href, texto) {
+
+    const criarLink = document.createElement('a')
+    criarLink.setAttribute('href', href)
+    criarLink.textContent = texto
+
+    return criarLink
+}
+
+// Mudando os textos no H2
+function mudarH2(elemento, num) {
+    elemento.textContent = `${num}. ${elemento.textContent}`
+    elemento.id = `h2${num}`
+    const voltar = f_criarLink('#', 'Voltar')
+    f_inserirDepois(elemento, voltar)
+}
+
+// Inserindo na h2 modificado na tela
+function f_inserirDepois(elemento, ref) {
+    const nextElement = elemento.nextElementSibling
+    elemento.parentElement.insertBefore(ref, nextElement)
+}
+
+
+  
